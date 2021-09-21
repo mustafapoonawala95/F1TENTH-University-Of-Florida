b6d5b34e2f44d9ada9b77ce3798fc6006c501d96
Mustafa <mustafapoonawala@ufl.edu>
2021-09-21 18:05:29 -0400

    More accurate lookahead point selection

--- Path_planning/catkin_ws/src/pure_pursuit_controller/src/pure_pursuit_controller_node.cpp
+++ Path_planning/catkin_ws/src/pure_pursuit_controller/src/pure_pursuit_controller_node.cpp
@@ -100,10 +100,10 @@ class pure_pursuit_controller {
         }
         //std::cout << "Adding first element to temp poses.\n";
         dense_temp_poses.push_back(temp_poses[0]);
-        float point_spacing = 0.1;
+        float point_spacing = 0.05;
         //std::cout << "First element added to temp poses.\n"; 
         for(int i=1;i<path_size_;i++){
-            while(dist(dense_temp_poses.back(),temp_poses[i])>=point_spacing){ // point_spacing was lookahead_distance_ earlier.
+            while(dist(dense_temp_poses.back(),temp_poses[i])>point_spacing){ // point_spacing was lookahead_distance_ earlier.
                 float DY = temp_poses[i].pose.position.y - dense_temp_poses.back().pose.position.y;
                 float DX = temp_poses[i].pose.position.x - dense_temp_poses.back().pose.position.x; 
                 float slope = DY/DX;
@@ -135,7 +135,7 @@ class pure_pursuit_controller {
         path_detected = true;
     }
 
-    void car_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& car_pose){
+    /*void car_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& car_pose){
         // Pure pursuit algorithm implemented here.
         if(path_detected){
             float car_x = car_pose->pose.position.x;
@@ -177,9 +177,9 @@ class pure_pursuit_controller {
                 }
 
             }
-            /*std::cout << "Current best waypoint distance is: " << current_best_waypoint_dist <<"\n";
-            std::cout << "Path seq. chosen to follow is: " << current_best_waypoint_seq << "\n";
-            std::cout << "the current best y is: " << curr_best_y_ << "\n";*/
+            //std::cout << "Current best waypoint distance is: " << current_best_waypoint_dist <<"\n";
+            //std::cout << "Path seq. chosen to follow is: " << current_best_waypoint_seq << "\n";
+            //std::cout << "the current best y is: " << curr_best_y_ << "\n";
 
             path_seq_ = current_best_waypoint_seq;    // keeping track of the seq of last waypoint chosen to follow.
             ackermann_msgs::AckermannDriveStamped drive_command;
@@ -197,7 +197,159 @@ class pure_pursuit_controller {
             }
 
             else{
+                std::cout << "The lookahead distance used is: " << current_best_waypoint_dist << "\n";
                 float steering_angle = 2*(curr_best_y_)/(current_best_waypoint_dist*current_best_waypoint_dist);
+                if(steering_angle > max_steering_angle_){
+                    steering_angle = max_steering_angle_;
+                    drive_command.drive.speed = slowest_speed_;
+                }
+                else if(steering_angle < -max_steering_angle_){
+                    steering_angle = -max_steering_angle_;
+                    drive_command.drive.speed = slowest_speed_;
+                }
+                else if(abs(steering_angle)>= 0.15){
+                    drive_command.drive.speed = mid_speed_;
+                    if(goal_closeness(std::make_pair(car_x,car_y)) == 2){
+                        drive_command.drive.speed = slowest_speed_;
+                    }
+                }
+                else {
+                    drive_command.drive.speed = max_speed_;
+                    if(goal_closeness(std::make_pair(car_x,car_y)) == 2){
+                        drive_command.drive.speed = slowest_speed_;
+                    }
+                }
+                drive_command.header.stamp = ros::Time::now();
+                drive_command.header.frame_id = "base_link";
+                drive_command.drive.steering_angle = steering_angle;
+                //std::cout << "The speed is commanded is:" << drive_command.drive.speed << "\n";
+                drive_command_publisher.publish(drive_command);
+                //std::cout << "Steering angle commanded is: " << steering_angle << "\n";
+            }
+        } // if(path_detected) block ends here. 
+        else{
+            //std::cout << "No path detected to track, so bye!\n";
+        }          
+    }*/
+
+    void car_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& car_pose){
+     // Pure pursuit algorithm implemented here.
+        if(path_detected){
+            double car_x = car_pose->pose.position.x;
+            double car_y = car_pose->pose.position.y;
+            double target_point_x = 0.0;
+            double target_point_y = 0.0; 
+            double target_point_dist = 0.0;
+            //float current_best_waypoint_dist = 99999.00;
+            //float curr_best_y_ = 99999.0;
+            geometry_msgs::TransformStamped map_to_car_frame;
+            while(!tf_buffer.canTransform("base_link", "map", ros::Time()));
+            try{
+                map_to_car_frame = tf_buffer.lookupTransform("base_link", "map", ros::Time(0)); // Getting latest transform.
+                //std::cout << "Current transform is: " << map_to_car_frame.transform.translation.x << ", " <<
+                //map_to_car_frame.transform.translation.y << "\n";
+            }
+            catch (tf2::TransformException &ex) {
+                std::cout << "blah blah\n";
+                ROS_WARN("%s",ex.what());
+                ros::Duration(1.0).sleep();
+            }
+            // Going over all path points in the path beyond the last path_seq_
+            bool intersection_found = false;
+            geometry_msgs::PointStamped target_point_map_frame;
+            geometry_msgs::PointStamped target_point_car_frame;
+            for(int i=path_seq_;i<(path_size_-1); i++){
+                double t1;
+                double t2;
+                double ex = path_to_track_map_frame_.poses[i].pose.position.x;
+                double ey = path_to_track_map_frame_.poses[i].pose.position.y;
+                double lx = path_to_track_map_frame_.poses[i+1].pose.position.x;
+                double ly = path_to_track_map_frame_.poses[i+1].pose.position.y;
+                double dx = lx - ex;
+                double dy = ly - ey;
+                double fx = ex - car_x;
+                double fy = ey - car_y;
+                double a = dx*dx + dy*dy;
+                double b = 2*(fx*dx + fy*dy);
+                double c = (fx*fx + fy*fy) - (lookahead_distance_*lookahead_distance_);
+                double discriminant = (b*b) - (4*a*c);
+                if(discriminant < 0){// Just chill
+                }
+                else{
+                    discriminant = sqrt(discriminant);
+                    t1 = (-b - discriminant)/(2*a);
+                    t2 = (-b + discriminant)/(2*a);
+                    if(t1 >= 0 && t1 <=1){
+                        target_point_map_frame.header.frame_id = "map";
+                        target_point_map_frame.point.x = ex + t1*dx;
+                        target_point_map_frame.point.y = ey + t1*dy;
+                        target_point_map_frame.point.z = 0.00;
+                        tf2::doTransform(target_point_map_frame, target_point_car_frame, map_to_car_frame);
+                        path_seq_ = i;
+                        intersection_found = true;
+                        break;
+                    }
+                    if(t2 >= 0 && t2 <=1){
+                        target_point_map_frame.header.frame_id = "map";
+                        target_point_map_frame.point.x = ex + t2*dx;
+                        target_point_map_frame.point.y = ey + t2*dy;
+                        target_point_map_frame.point.z = 0.00;
+                        tf2::doTransform(target_point_map_frame, target_point_car_frame, map_to_car_frame);
+                        path_seq_ = i;
+                        intersection_found = true;
+                        break;
+                    }
+                }
+            }
+            if(intersection_found){
+                target_point_x = target_point_car_frame.point.x;
+                target_point_y = target_point_car_frame.point.y; 
+                target_point_dist = sqrt(target_point_x*target_point_x + target_point_y*target_point_y);
+                //std::cout << "Intersection with path found and distance from car is: " << target_point_dist << "\n";
+            }
+            else{
+                std::cout << "No intersection between lookahead radius cirlcle and path found.\n";
+                // Since no point of intersection found. Finding the closest point on the path and tracking that.
+                double current_best_waypoint_dist = 999999.00;
+                double current_best_waypoint_seq;
+                for(int j = path_seq_; j<path_size_; j++){
+                    double curr_x = path_to_track_map_frame_.poses[j].pose.position.x;
+                    double curr_y = path_to_track_map_frame_.poses[j].pose.position.y;
+                    //double curr_z = path_to_track_map_frame_.poses[j].pose.position.z;
+                    double delta_x = curr_x - car_x;
+                    double delta_y = curr_y - car_y;
+                    double current_waypoint_dist = sqrt((delta_x*delta_x) + (delta_y*delta_y));
+                    if(current_waypoint_dist < current_best_waypoint_dist){
+                        current_best_waypoint_dist = current_waypoint_dist;
+                        current_best_waypoint_seq = j;
+                    }
+                }
+                target_point_map_frame.header.frame_id = "map";
+                target_point_map_frame.point.x = path_to_track_map_frame_.poses[current_best_waypoint_seq].pose.position.x;
+                target_point_map_frame.point.y = path_to_track_map_frame_.poses[current_best_waypoint_seq].pose.position.y;
+                target_point_map_frame.point.z = path_to_track_map_frame_.poses[current_best_waypoint_seq].pose.position.z;
+                tf2::doTransform(target_point_map_frame, target_point_car_frame, map_to_car_frame);
+
+                target_point_x = target_point_car_frame.point.x;
+                target_point_y = target_point_car_frame.point.y; 
+                target_point_dist = sqrt(target_point_x*target_point_x + target_point_y*target_point_y);
+                path_seq_ = current_best_waypoint_seq;
+            }   
+            
+            ackermann_msgs::AckermannDriveStamped drive_command;
+
+            if(goal_closeness(std::make_pair(car_x,car_y)) == 1){
+                stop_car();
+                // Once goal reached resetting class members so that next path can be follwed correctly.
+                path_seq_ = 0;
+                path_detected = false;
+                target_point_x = 0.0;
+                target_point_y = 0.0; 
+                target_point_dist = 0.0;
+            }
+            else{
+                std::cout << "The lookahead distance used is: " << target_point_dist << "\n";
+                float steering_angle = 2*(target_point_y)/(target_point_dist*target_point_dist);
                 if(steering_angle > max_steering_angle_){
                     steering_angle = max_steering_angle_;
                     drive_command.drive.speed = slowest_speed_;
